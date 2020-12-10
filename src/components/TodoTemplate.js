bb533bc9bbaf75668f558abb7a53f235322c395a
sunyoung <sunnyk5780@gmail.com>
2020-12-11 00:01:11 +0900

    Add: TodoTemplate.scss

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -4,8 +4,10 @@ import './TodoTemplate.scss';
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <div className="app-title">일정 관리</div>
+      <div className="app-title">Todo List</div>
       <div className="content">{children}</div>
     </div>
   );
 };
+
+export default TodoTemplate;
