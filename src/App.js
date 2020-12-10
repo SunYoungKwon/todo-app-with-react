08edacf63c30f3d6a3dfc36d31d9d85b9bcc2241
sunyoung <sunnyk5780@gmail.com>
2020-12-11 01:32:05 +0900

    Feat: print todolist with todos data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Study Javascript',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'Study React',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: 'Study Typescript',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInput />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
