a103349e38e5c5a029da6f7f343b6a158d64045f
sunyoung <sunnyk5780@gmail.com>
2020-12-17 01:43:23 +0900

    Refactor: improve performance with useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
-const createBulkTodos = (numOfTodo) => {
+const INIT_NUM_OF_TODO = 2500;
+
+const createBulkTodos = () => {
   const todos = [];
 
-  for (let i = 0; i < numOfTodo; i++) {
+  for (let i = 1; i <= INIT_NUM_OF_TODO; i++) {
     todos.push({
       id: i,
       text: `Todo ${i}`,
@@ -17,10 +19,26 @@ const createBulkTodos = (numOfTodo) => {
   return todos;
 };
 
-const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos(2500));
+const todoReducer = (todos, action) => {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
 
-  const nextId = useRef(4);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+
+    case 'TOGGLE':
+      return todos.map((todo) => (todo.id === action.id ? { ...todo, checked: !todo.checked } : todo));
+
+    default:
+      return todos;
+  }
+};
+
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
+  const nextId = useRef(INIT_NUM_OF_TODO + 1);
 
   const onInsert = useCallback((text) => {
     const todo = {
@@ -29,16 +47,16 @@ const App = () => {
       checked: false,
     };
 
-    setTodos((todos) => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
     nextId.current++;
   }, []);
 
   const onRemove = useCallback((id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
   }, []);
 
   const onToggle = useCallback((id) => {
-    setTodos((todos) => todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo)));
+    dispatch({ type: 'TOGGLE', id });
   }, []);
 
   return (
