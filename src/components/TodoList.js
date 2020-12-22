e4885b382f1e488ba53a18f7f750a4d79bdbc92d
sunyoung <sunnyk5780@gmail.com>
2020-12-23 00:34:57 +0900

    Refactor: improve TodoList with react-virtualized

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+
+      return <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style} />;
+    },
+    [onRemove, onToggle, todos],
+  );
+
   return (
-    <ul className="TodoList">
-      {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-      ))}
-    </ul>
+    <List
+      className="TodoList"
+      width={512}
+      height={650}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    />
   );
 };
 
